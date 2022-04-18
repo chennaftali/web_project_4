98b30e711cac2ea9fe205a6b9676be975735e622
chen-naftali <chennaftali1@gmail.com>
2022-04-18 15:12:27 +0300

    my 4 project1

--- script/index.js
+++ script/index.js
@@ -1,23 +1,30 @@
+const popup = document.querySelector('.popup');
+const openPopupButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close');
 const inputName = document.querySelector('.popup__content_type_name');
 const inputOccupation = document.querySelector('.popup__content_type_about-me');
-const Popup = document.querySelector('.popup');
+const profileName = document.querySelector('.profile__name');
+const profileOccupation = document.querySelector('.profile__explorer');
+const formSubmit = document.querySelector('.popup__form');
+const form = document.querySelector('popup__form');
 
-const openPopupButton = document.querySelector('.profile__edit-button');
-const closePopupButton = document.querySelector('popup__close');
 
-
-function openPopup() {
-    Popup.classList.add("popup_open");
+function openPopup () {
+    popup.classList.add('popup_open');
     inputName.value = profileName.textContent;
     inputOccupation.value = profileOccupation.textContent;
 }
-//function closePopup() {
-//popup.classList.remove("popup_open");
-//}
-//function savePopup(e) {
-    //closePopup();
-    //e.preventDefault();
-    //profileName.textContent = inputName.value;
-    //profileOccupation.textContent = inputOccupation.value;
-//}
-openPopupButton.addEventListener("click", openPopup);
+
+function closePopup() {
+    popup.classList.remove('popup_open');
+}
+
+function savePopup(event) {
+    closePopup();
+    event.preventDefault();
+    profileName.textContent = inputName.value;
+    profileOccupation.textContent = inputOccupation.value;
+}
+openPopupButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+formSubmit.addEventListener('submit', savePopup);
